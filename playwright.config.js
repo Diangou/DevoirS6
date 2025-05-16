84efe1cee9efdc80503ea10ec0a8fb1d35ed601f
Diangou <diangou.cmr09@gmail.com>
2025-05-16 19:13:49 +0200

    Test e2e

--- playwright.config.js
+++ playwright.config.js
@@ -20,11 +20,9 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 2 : undefined,
+    workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: process.env.CI ? [["dot"], ["github"], ["html"]] : "html",
-    /* 5 minutes max par fichier */
-    timeout: 5 * 60 * 1000, 
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
@@ -32,8 +30,6 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
-        timeout: 60000, // 60s max par test
-
     },
 
     /* Configure projects for major browsers */
