99726f38110697e4f5c17abff8a21c3c93d242e2
AODAMA <antoinemoncada92@gmail.com>
2025-06-10 21:49:05 +0200

    jaquettesModales

--- src/api/index.js
+++ src/api/index.js
@@ -5,4 +5,16 @@ export * from "./tyradex";
 export * from "./tcgdex"; // Ajout du fichier tcgdex.js
 export * from "./apiGitHub.js"; // Ajout du fichier tcgdex.js
 
+import axios from "axios";
+
+export const fetchGameCovers = async () => {
+    try {
+        const response = await axios.get("http://localhost:3000/list-jaquettes");
+        return response.data;
+    } catch (error) {
+        console.error("Erreur lors de la récupération des jaquettes:", error);
+        return [];
+    }
+};
+
 
